957f562f9b575d7b0f9767849cdc273274441ac3
LuisGG34 <luis.gutierrezgo@mail.udp.cl>
2024-05-24 22:58:33 -0400

    se agrega el readme

---
+++ README.md
@@ -0,0 +1,2 @@
+DEPLOY: https://luisgg34.github.io/Desafio-9-Animaciones-CSS/
+LINK GIT: https://github.com/LuisGG34/Desafio-9-Animaciones-CSS
--- assets/js/misScript.js
+++
@@ -1,7 +0,0 @@
-function volverHome(){
-    window.location.href = 'realMandril.html';
-}
-
-function paginaHimno(){
-    window.location.href = 'himnoClub.html';
-}
